178efcd18947ad3566e9571731cb6624be4418b4
daisukemizukami <d.increment@gmail.com>
2019-09-30 15:29:17 +0900

    add remaining cubelayers rotation

--- Assets/Scripts/RubicCube.cs
+++ Assets/Scripts/RubicCube.cs
@@ -34,7 +34,11 @@ public class RubicCube : MonoBehaviour
         easing = Easings.CubicEaseInOut;
     }
 
-    public void RotateFrontFace()
+
+    /// <summary>
+    /// rotate test method 
+    /// </summary>
+    public void PressButtonF()
     {
         if (!isRotating)
         {
@@ -42,13 +46,77 @@ public class RubicCube : MonoBehaviour
             RotateCW(ELayer.F);
         }
     }
-    public void RotateRightFace()
+    public void PressButtonR()
     {
         if (!isRotating)
         {
             isRotating = true;
 
             RotateCW(ELayer.R);
+        }
+    }
+
+    public void PressButtonU()
+    {
+        if (!isRotating)
+        {
+            isRotating = true;
+
+            RotateCW(ELayer.U);
+        }
+    }
+    public void PressButtonL()
+    {
+        if (!isRotating)
+        {
+            isRotating = true;
+
+            RotateCW(ELayer.L);
+        }
+    }
+    public void PressButtonB()
+    {
+        if (!isRotating)
+        {
+            isRotating = true;
+
+            RotateCW(ELayer.B);
+        }
+    }
+    public void PressButtonD()
+    {
+        if (!isRotating)
+        {
+            isRotating = true;
+
+            RotateCW(ELayer.D);
+        }
+    }
+    public void PressButtonM()
+    {
+        if (!isRotating)
+        {
+            isRotating = true;
+
+            RotateCW(ELayer.M);
+        }
+    }
+    public void PressButtonE()
+    {
+        if (!isRotating)
+        {
+            isRotating = true;
+
+            RotateCW(ELayer.E);
+        }
+    }
+    public void PressButtonS()
+    {
+        if (!isRotating)
+        {
+            isRotating = true;
+
+            RotateCW(ELayer.S);
         }
     }
 
@@ -65,6 +133,41 @@ public class RubicCube : MonoBehaviour
                 Debug.Log("R");
                 StartCoroutine(RotateCube(R, _count, easing, Vector3.right));
                 R = R.RotateCW();
+                break;
+            case ELayer.U:
+                Debug.Log("U");
+                StartCoroutine(RotateCube(U, _count, easing, Vector3.up));
+                U = U.RotateCW();
+                break;
+            case ELayer.L:
+                Debug.Log("L");
+                StartCoroutine(RotateCube(L, _count, easing, Vector3.right));
+                L = L.RotateCW();
+                break;
+            case ELayer.B:
+                Debug.Log("B");
+                StartCoroutine(RotateCube(B, _count, easing, Vector3.back));
+                B = B.RotateCW();
+                break;
+            case ELayer.D:
+                Debug.Log("D");
+                StartCoroutine(RotateCube(D, _count, easing, Vector3.up));
+                D = D.RotateCW();
+                break;
+            case ELayer.M:
+                Debug.Log("M");
+                StartCoroutine(RotateCube(M, _count, easing, Vector3.right));
+                M = M.RotateCW();
+                break;
+            case ELayer.E:
+                Debug.Log("E");
+                StartCoroutine(RotateCube(E, _count, easing, Vector3.up));
+                E = E.RotateCW();
+                break;
+            case ELayer.S:
+                Debug.Log("S");
+                StartCoroutine(RotateCube(S, _count, easing, Vector3.back));
+                S = S.RotateCW();
                 break;
         }
 
@@ -135,14 +238,97 @@ public class RubicCube : MonoBehaviour
         }
     }
 
-    public CubeLayer U; // upper face (around y axis)
-    public CubeLayer L; // left face (around x axis)
-
-    public CubeLayer B; // back face (around z axis)
-    public CubeLayer D; // down face (around y axis)
-    public CubeLayer M; // middle layer around x-axis
-    public CubeLayer E; // middle layer around y-axis
-    public CubeLayer S; // middle layer around z-axis
+    public CubeLayer U // upper face (around y axis)
+    {
+        get
+        {
+            return new CubeLayer(cubes[22], cubes[24], cubes[25], cubes[26], cubes[23], cubes[20], cubes[19], cubes[18], cubes[21]);
+        }
+        set
+        {
+            cubes[24] = value.l0; cubes[25] = value.l1; cubes[26] = value.l2;
+            cubes[21] = value.l7; cubes[22] = value.lc; cubes[23] = value.l3;
+            cubes[18] = value.l6; cubes[19] = value.l5; cubes[20] = value.l4;
+        }
+    }
+    public CubeLayer L // left face (around x axis)
+    {
+        get
+        {
+            return new CubeLayer(cubes[12], cubes[18], cubes[21], cubes[24], cubes[15], cubes[6], cubes[3], cubes[0], cubes[9]);
+        }
+        set
+        {
+            cubes[18] = value.l0; cubes[21] = value.l1; cubes[24] = value.l2;
+            cubes[ 9] = value.l7; cubes[12] = value.lc; cubes[15] = value.l3;
+            cubes[ 0] = value.l6; cubes[ 3] = value.l5; cubes[ 6] = value.l4;
+        }
+    }
+    public CubeLayer B // back face (around z axis)
+    {
+        get
+        {
+            return new CubeLayer(cubes[16], cubes[24], cubes[25], cubes[26], cubes[17], cubes[8], cubes[7], cubes[6], cubes[15]);
+        }
+        set
+        {
+            cubes[24] = value.l0; cubes[25] = value.l1; cubes[26] = value.l2;
+            cubes[15] = value.l7; cubes[16] = value.lc; cubes[17] = value.l3;
+            cubes[ 6] = value.l6; cubes[ 7] = value.l5; cubes[ 8] = value.l4;
+        }
+    }
+    public CubeLayer D // down face (around y axis)
+    {
+        get
+        {
+            return new CubeLayer(cubes[4], cubes[6], cubes[7], cubes[8], cubes[5], cubes[2], cubes[1], cubes[0], cubes[3]);
+        }
+        set
+        {
+            cubes[ 6] = value.l0; cubes[ 7] = value.l1; cubes[ 8] = value.l2;
+            cubes[ 3] = value.l7; cubes[ 4] = value.lc; cubes[ 5] = value.l3;
+            cubes[ 0] = value.l6; cubes[ 1] = value.l5; cubes[ 2] = value.l4;
+        }
+    }
+    public CubeLayer M // middle layer around x-axis
+    {
+        get
+        {
+            return new CubeLayer(cubes[13], cubes[19], cubes[22], cubes[25], cubes[16], cubes[7], cubes[4], cubes[1], cubes[10]);
+        }
+        set
+        {
+            cubes[19] = value.l0; cubes[22] = value.l1; cubes[25] = value.l2;
+            cubes[10] = value.l7; cubes[13] = value.lc; cubes[16] = value.l3;
+            cubes[1] = value.l6; cubes[4] = value.l5; cubes[7] = value.l4;
+        }
+    }
+    public CubeLayer E // middle layer around y-axis
+    {
+        get
+        {
+            return new CubeLayer(cubes[13], cubes[15], cubes[16], cubes[17], cubes[14], cubes[11], cubes[10], cubes[9], cubes[12]);
+        }
+        set
+        {
+            cubes[15] = value.l0; cubes[16] = value.l1; cubes[17] = value.l2;
+            cubes[12] = value.l7; cubes[13] = value.lc; cubes[14] = value.l3;
+            cubes[ 9] = value.l6; cubes[10] = value.l5; cubes[11] = value.l4;
+        }
+    }
+    public CubeLayer S // middle layer around z-axis
+    {
+        get
+        {
+            return new CubeLayer(cubes[13], cubes[21], cubes[22], cubes[23], cubes[14], cubes[5], cubes[4], cubes[3], cubes[12]);
+        }
+        set
+        {
+            cubes[21] = value.l0; cubes[22] = value.l1; cubes[23] = value.l2;
+            cubes[12] = value.l7; cubes[13] = value.lc; cubes[14] = value.l3;
+            cubes[ 3] = value.l6; cubes[ 4] = value.l5; cubes[ 5] = value.l4;
+        }
+    }
 
 
 }
