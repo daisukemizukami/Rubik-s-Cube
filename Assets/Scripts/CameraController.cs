d9df24459cf8082a6cc62f8dcd9bae23b07f83b8
daisukemizukami <d.increment@gmail.com>
2019-10-04 00:54:10 +0900

    add ristriction to camera zoom movement

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -2,65 +2,63 @@
 using UnityEngine;
 using TouchScript.Gestures.TransformGestures;
 
-    public class CameraController : MonoBehaviour
+public class CameraController : MonoBehaviour
+{
+    public ScreenTransformGesture TwoFingerMoveGesture;
+    public ScreenTransformGesture OneFingerMoveGesture;
+    public float RotationSpeed = 10f;
+    public float ZoomSpeed = 1f;
+
+    [SerializeField] Transform targetobj;
+    [SerializeField] Transform cam;
+
+
+    private void OnEnable()
     {
-        public ScreenTransformGesture TwoFingerMoveGesture;
-        public ScreenTransformGesture ManipulationGesture;
-        public float PanSpeed = 200f;
-        public float RotationSpeed = 200f;
-        public float ZoomSpeed = 10f;
+        TwoFingerMoveGesture.Transformed += twoFingerTransformHandler;
+        OneFingerMoveGesture.Transformed += OneFingerMoveTransformedHandler;
+    }
 
-        [SerializeField] Transform targetobj;
-        [SerializeField] Transform cam;
+    private void OnDisable()
+    {
+        TwoFingerMoveGesture.Transformed -= twoFingerTransformHandler;
+        OneFingerMoveGesture.Transformed -= OneFingerMoveTransformedHandler;
+    }
 
-    private void Awake()
-        {
-            
-        }
+    private void OneFingerMoveTransformedHandler(object sender, System.EventArgs e)
+    {
+        cam.RotateAround(targetobj.position, Vector3.up, OneFingerMoveGesture.DeltaPosition.x * Time.deltaTime * RotationSpeed);
+        cam.RotateAround(targetobj.position, cam.right, -OneFingerMoveGesture.DeltaPosition.y * Time.deltaTime * RotationSpeed);
 
-        private void OnEnable()
-        {
-            TwoFingerMoveGesture.Transformed += twoFingerTransformHandler;
-            ManipulationGesture.Transformed += manipulationTransformedHandler;
-        }
-
-        private void OnDisable()
-        {
-            TwoFingerMoveGesture.Transformed -= twoFingerTransformHandler;
-            ManipulationGesture.Transformed -= manipulationTransformedHandler;
-        }
-
-        private void manipulationTransformedHandler(object sender, System.EventArgs e)
-        {
-
-        //Debug.Log(ManipulationGesture.DeltaPosition.x +" " + ManipulationGesture.DeltaPosition.y );
-
-        // targetの位置のY軸を中心に、回転（公転）する
-        cam.RotateAround(targetobj.position, Vector3.up, ManipulationGesture.DeltaPosition.x * Time.deltaTime * RotationSpeed);
-        // カメラの垂直移動（※角度制限なし、必要が無ければコメントアウト）
-        cam.RotateAround(targetobj.position, cam.right, -ManipulationGesture.DeltaPosition.y * Time.deltaTime * RotationSpeed);
-
-        Debug.Log("one finger  " );
-
-        //var rotation = Quaternion.Euler(ManipulationGesture.DeltaPosition.y / Screen.height * RotationSpeed,
-        //    -ManipulationGesture.DeltaPosition.x / Screen.width * RotationSpeed,
-        //    ManipulationGesture.DeltaRotation);
-        //targetobj.localRotation *= rotation;
-
-        //cam.transform.localPosition += Vector3.forward * (ManipulationGesture.DeltaScale - 1f) * ZoomSpeed;
+        //Debug.Log("one finger  " );
     }
 
     private void twoFingerTransformHandler(object sender, System.EventArgs e)
+    {
+
+        Debug.Log(cam.transform.position.magnitude);
+
+        //-7 -20
+        if(TwoFingerMoveGesture.DeltaPosition.y < 0)
         {
-        //cam.localPosition += targetobj.rotation * TwoFingerMoveGesture.DeltaPosition * PanSpeed;
-         cam.Translate(Vector3.forward * TwoFingerMoveGesture.DeltaPosition.y * PanSpeed);
-        //cam.localPosition =
-        //    new Vector3
-        //    (
-        //        0,
-        //        0,
-        //        cam.localPosition.z  +TwoFingerMoveGesture.DeltaPosition.x * PanSpeed
-            //);
-        Debug.Log("two finger  " + TwoFingerMoveGesture.DeltaPosition);
+            if (cam.transform.position.magnitude < 20f)
+            {
+                cam.Translate(Vector3.forward * TwoFingerMoveGesture.DeltaPosition.y * ZoomSpeed);
+                Debug.Log("TwoFingerMoveGesture.DeltaPosition.y　上" + TwoFingerMoveGesture.DeltaPosition.y);
+            }
+        
+        }
+        else
+        {
+            if (cam.transform.position.magnitude > 7f)
+            {
+                cam.Translate(Vector3.forward * TwoFingerMoveGesture.DeltaPosition.y * ZoomSpeed);
+                Debug.Log("TwoFingerMoveGesture.DeltaPosition.y 下" + TwoFingerMoveGesture.DeltaPosition.y);
+
+            }
+        
+        }
+
+        //Debug.Log("two finger  " + TwoFingerMoveGesture.DeltaPosition);
     }
     }
